a7eb60601b6c5b2c96d7db48e8ddf6ef2a435731
liaowen <liaowen@liaowens-MacBook-Pro.local>
2021-10-20 23:14:32 -0700

    changing port

--- server.js
+++ server.js
@@ -91,6 +91,6 @@ app.put('/image', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-  console.log('app is runing on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('app is runing on port ${process.env.PORT}');
 })
